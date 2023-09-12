d5c52de66ac53dfecf91dcb50a6156c63219c3d2
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-09-11 21:02:08 -0500

    Added the home.js file

---
+++ esparto/src/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import SplitPane from "react-split-pane";
+ export default function Home() 
+ {
+    return ( 
+        <div className="Home">
+            <h2>Welcome to Esparto</h2>
+        </div>
+        );
+} 
