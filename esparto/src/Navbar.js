605bbf0c0183f9dc8342c90fd96fcfd827b18eb1
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-09-13 15:24:54 -0500

    Update Navbar.js by Sailaja Lakkakula

--- esparto/src/Navbar.js
+++ esparto/src/Navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar(){
     return <nav className="nav">
-          <a className="siteTittle" href="/" > LOGO  ESPARTO</a>
+          <a className="siteTittle" href="/" >LOGO  ESPARTO</a>
         <ul>
         <li> <a href="/">HOME</a></li>
         </ul>
