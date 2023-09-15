6ce2729d1eb03c90cdba5da060209379874fc1fd
AjayKumar1403 <ajaykumar.us21@gmail.com>
2023-09-15 09:38:24 -0500

    linked the about us page with navigation bar inside the project

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a className="siteTittle" href="/" > LOGO  ESPARTO</a>
+        <ul>
+            
+            <li> <a href="/">HOME</a></li>
+            <li> <a href="/aboutus">ABOUT US</a></li>
+            <li>  <a href="/paintings">PAINTINGS</a></li>
+            <li>  <a href="/artAdviosry">ART ADVISORY</a></li>
+            <li>  <a href="/blogs">BLOGS</a></li>
+            <li> <a href="/contact">CONTACT US</a></li>
+            
+        </ul>
+    </nav>
+}
