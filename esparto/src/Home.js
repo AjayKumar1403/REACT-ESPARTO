d15fc39d882298261f7d3fd058da7f949e0142cd
ManojThella <manojthella98@gmail.com>
2023-09-15 19:59:59 -0500

    updated Home page by Manoj Thella

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -5,9 +5,7 @@ import React, { useState } from "react";
     return ( 
         <div className="Home">
             <h1>Welcome to Our Esparto</h1>
-            <h2> We can have a major look in detail with the Artist Name etc.... 
-                with the price quoted and the types of paints/materials used for Art which depicts 
-                the real-time Process and modifications as well feedback is also allowed in our Page</h2>
+            <p>We can also have a major look in detail with the Artist Name, with the price quoted etc... and mentions the types of paints/materials used for Art which depicts the real-time Process and modifications as well feedback is also allowed in our Page </p> 
         </div>
         );
 } 
