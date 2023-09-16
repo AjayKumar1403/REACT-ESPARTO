495ba8838574b2db3573bc7a6bdfb8c979e994f9
Varshakalva <kalvakuntla.varsha@gmail.com>
2023-09-15 19:52:17 -0500

    Given an update in Detailed Process and Artist Contact Method which will be helpful to the Users!

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -5,6 +5,9 @@ import React, { useState } from "react";
     return ( 
         <div className="Home">
             <h1>Welcome to Our Esparto</h1>
+            <h2> We can have a major look in detail with the Artist Name etc.... 
+                with the price quoted and the types of paints/materials used for Art which depicts 
+                the real-time Process and modifications as well feedback is also allowed in our Page</h2>
         </div>
         );
 } 
