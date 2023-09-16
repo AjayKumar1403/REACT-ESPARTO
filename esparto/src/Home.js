5fbccbdd11aedcc7804ca691933eda6f683fdb44
Sai Chaitanya Vishnu Kanth Inavolu <119522543+Sai-1305@users.noreply.github.com>
2023-09-15 22:26:49 -0500

    modified the contents of About.js and Home.js - Sai Chaitanya Vishnu Kanth Inavolu

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -4,7 +4,8 @@ import React, { useState } from "react";
  {
     return ( 
         <div className="Home">
-            <h2>Welcome to Our Esparto</h2> 
+            <h2>Welcome to Our Esparto!</h2> 
+            <h3>A digital platform for Art Gallery is here.</h3>
             <p>We can also have a major look in detail with the Artist Name, with the price quoted etc... and mentions the types of paints/materials used for Art which depicts the real-time Process and modifications as well feedback is also allowed in our Page </p> 
         </div>
         );
