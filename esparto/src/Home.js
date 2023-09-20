2b31ef3a2f18c14d67da1d849333ce12093a30da
ManojThella <manojthella98@gmail.com>
2023-09-20 18:26:57 -0500

    Updates made in home.js by Manoj Thella

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
     return ( 
         <div className="Home">
             <h2>Welcome to Our Esparto!</h2> 
-            <h3>A digital platform for Art Gallery is here.</h3>
+            <h5>A digital platform for Art Gallery is here.</h5>
             <p>We can also have a major look in detail with the Artist Name, with the price quoted etc... and mentions the types of paints/materials used for Art which depicts the real-time Process and modifications as well feedback is also allowed in our Page </p> 
         </div>
         );
