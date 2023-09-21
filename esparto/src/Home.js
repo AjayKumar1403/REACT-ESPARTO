1930fe841e29927661156a58d2b17307ec3cf303
SahithiKasarapu <sahithikasarapu@gmail.com>
2023-09-20 23:18:04 -0500

    Update the heading and style of the font in the home page - Sahithi Kasarapu

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -7,8 +7,8 @@ import React, { useState } from "react";
     return ( 
         <div className="Home">
             
-            <h2>Welcome to Our Esparto!</h2> 
-            <h6>A digital platform for Art Gallery is here.</h6>
+            <h2>Welcome to Our <b>Esparto!</b></h2> 
+            <h5>A digital platform for Art Gallery is here.</h5>
             <p>We can also have a major look in detail with the Artist Name, 
                 with the price quoted etc... and mentions the types of 
                 paints/materials used for Art which depicts the real-time 
