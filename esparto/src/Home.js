501d1f1efbc44bc1d1791169ea6c09d44e921204
ManojThella <manojthella98@gmail.com>
2023-09-25 21:05:18 -0500

    Update Home.js

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -8,19 +8,11 @@ import img3 from '../src/assets/image4.jpg';
 
  export default function Home() 
  {
-
-
     const imgList = [img0, img1, img2, img3];
 
     const [index, setIndex] = useState(0);
 
     const onClickForward = () => {
-        
-        if (index + 1 === imgList.length) {
-            setIndex(0);
-        } else {
-            setIndex(index + 1);
-        }
         
     };
 
@@ -29,45 +21,11 @@ import img3 from '../src/assets/image4.jpg';
     };
   
     return ( 
-        <div className="Home">
-            
-            <h2>Welcome to Our <b>Esparto!</b></h2> 
-            <h5>A digital platform for Art Gallery is here.</h5>
-            <p>
-                We can also have a major look in detail with the Artist Name, 
-                with the price quoted etc... and mentions the types of 
-                paints/materials used for Art which depicts the real-time 
-                Process and modifications as well feedback is also allowed 
-                in our Page 
-            </p> 
-            
-         <p>
-            An artist is a person engaged in an activity related to creating 
-            art, practicing the arts, or demonstrating an art. The common 
-            usage in both everyday speech and academic discourse refers to 
-            a practitioner in the visual arts only. However, the term is also 
-            often used in the entertainment business, especially in a business 
-            context, for musicians and other performers (although less often 
-            for actors). "Artiste" (French for artist) is a variant used in 
-            English in this context, but this use has become rare. The use of 
-            the term "artist" to describe writers is valid, but less common, 
-            and mostly restricted to contexts such as critics' reviews.
-        </p>
-
-            
-
-            <SplitPane
-                split="vertical"
-                minSize={100}
-                maxSize={-100}
-                defaultSize={"50%"}
-            >
-               
-            </SplitPane>
-
-           
-        </div>
-
-        
+        <div className="leftPane">
+                    <h2 className="hdr">WELCOME TO ESPARTO</h2>
+                    <div className="imageButtons">
+                        <button onClick={onClickBackward}><img className="navButtons" src={require("../src/assets/back.png")}></img></button>
+                    </div>
+                    </div>
         );
 } 
