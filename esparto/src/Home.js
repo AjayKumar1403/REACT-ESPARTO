06da0bd60e59c60ea4ae87379976181d6e5d2990
AjayKumar1403 <ajaykumar.us21@gmail.com>
2023-09-27 17:50:28 -0500

    adjusted the overlaping of the components

--- esparto/src/Home.js
+++ esparto/src/Home.js
@@ -30,35 +30,43 @@ import img3 from '../src/assets/image4.jpg';
     };
   
     return ( 
-
-    <SplitPane
-    split="vertical"
-    minSize={100}
-    maxSize={-100}
-    defaultSize={"50%"}
-    >
-        <div className="leftPane">
-                    <h2 className="hdr">WELCOME TO ESPARTO</h2>
-                    <div className="imageButtons">
-                        <button onClick={onClickBackward}><img className="navButtons" src={require("../src/assets/back.png")}></img></button>
-                    </div>
-        </div>
-        <div className="rightPane">
-                    <div className="imageContainer">
-                        <img className="image" src={imgList[index]} alt="image" />
+    <div>
+    <div className="Home">
+        <SplitPane
+        split="vertical"
+        minSize={100}
+        maxSize={-100}
+        defaultSize={"50%"}
+        >
+            <div className="leftPane">
+                        <h2 className="hdr">WELCOME TO ESPARTO</h2>
                         <div className="imageButtons">
-                            
-                            <button onClick={onClickForward}><img className="navButtons" src={require("../src/assets/forward.png")}></img></button>
+                            <button onClick={onClickBackward}><img className="navButtons" src={require("../src/assets/back.png")}></img></button>
                         </div>
-                    </div>
+            </div>
+            <div className="rightPane">
+                        <div className="imageContainer">
+                            <img className="image" src={imgList[index]} alt="image" />
+                            <div className="imageButtons">
+                                
+                                <button onClick={onClickForward}><img className="navButtons" src={require("../src/assets/forward.png")}></img></button>
+                            </div>
+                        </div>
+            </div>
+            <div className="middleSection">
+                    {
+                    /* All the features go here  */
+                    }
+            </div>
+
+            </SplitPane>
+
+           
         </div>
         <div className="middleSection">
-                {
-                /* All the features go here  */
-                }
+                {/* All the features go here  */}
+            </div>
         </div>
-
-        </SplitPane>
         );
         
         
