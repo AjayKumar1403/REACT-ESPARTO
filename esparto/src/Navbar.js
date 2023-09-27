2646b9df8da4c5c0b103127d0fd1e3c6f2ac73b3
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-09-27 17:52:13 -0500

    Updated the display of the Page with appropriate bars

--- esparto/src/Navbar.js
+++ esparto/src/Navbar.js
@@ -1,8 +1,16 @@
 export default function Navbar(){
     return <nav className="nav">
-          <a className="siteTittle" href="/" > LOGO  ESPARTO</a>
+          <a className="siteTittle" href="/" >LOGO  ESPARTO</a>
         <ul>
-        <li> <a href="/">HOME</a></li>
+        <li> 
+          <a href="/">HOME Page</a>
+        </li>
+        <li> 
+          <a href="/aboutus">ABOUT US Page</a>
+        </li>
+        <li> 
+          <a href="/contactus">CONTACT US</a>
+        </li>
         </ul>
 
         </nav>
