e0373ef31d8f199eb820ade0579c2bacb14d8a24
AjayKumar1403 <ajaykumar.us21@gmail.com>
2023-09-29 21:14:10 -0500

    addded the code in about us page which gives the description of developer Ajay

--- esparto/src/About.js
+++ esparto/src/About.js
@@ -3,8 +3,23 @@ export default function About(){
     <div>
         <h2 className="AboutHeader">
             Presenting you, one of the finest collections...
-            World's best paintings are found here. Open the doors to your art collection.
-        </h2>
+            World's best paintings are found here. Open the doors to your art collection.     </h2>
+
+
+            <h2>About the Developers</h2>
+            <p>GitHub Username: AjayKumar1403</p>
+
+            <p>GitHub profile</p>
+
+            <p>Strengths</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>Angular</p>
+            <p>React JS</p>
+            <p>Node JS</p>
+
+   
     </div>
     )
 }
