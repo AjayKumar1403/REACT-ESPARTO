092f64e550bd3cd973a0eec16b5e348888b9744b
Sai Chaitanya Vishnu Kanth Inavolu <119522543+Sai-1305@users.noreply.github.com>
2023-10-02 22:28:01 -0500

    Sai Chaitanya Vishnu Kanth Inavolu:Added ContactUs component and installed all the required dependencies

---
+++ esparto/src/components/ContactUs.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ContactUs = () => {
+    return (
+        <div id="ContactUs">
+            <table id="contactTable">
+                <tr id="tableHeadRow">
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                <tr>
+                    <td>Ajay Kumar Dhonthoju</td>
+                    <td><a href="mailto:s558637@nwmissouri.edu">s558637@nwmissouri.edu</a></td>
+                </tr>
+                <tr>
+                    <td>Sahithi Kasarapu</td>
+                    <td><a href="s559488@nwmissouri.edu">s559488@nwmissouri.edu</a></td>
+                </tr>
+                <tr>
+                    <td>Varsha Kalvakuntla</td>
+                    <td><a href="s559976@nwmissouri.edu">s559976@nwmissouri.edu</a></td>
+                </tr>
+                <tr>
+                    <td>Sailaja Lakkakula</td>
+                    <td><a href="s562042@nwmissouri.edu">s562042@nwmissouri.edu</a></td>
+                </tr>
+                <tr>
+                    <td>Sai Chaitanya Vishnu Kanth Inavolu</td>
+                    <td><a href="s560446@nwmissouri.edu">s560446@nwmissouri.edu</a></td>
+                </tr>
+                <tr>
+                    <td>Manoj Thella</td>
+                    <td><a href="s559494@nwmissouri.edu">s559494@nwmissouri.edu</a></td>
+                </tr>
+            </table>
+        </div>
+    );
+
+};
+
+export default ContactUs;
