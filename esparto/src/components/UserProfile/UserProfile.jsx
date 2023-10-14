7caf7dfe7ccf82e75fb76a43598b1357df86f10d
SahithiKasarapu <sahithikasarapu@gmail.com>
2023-10-13 22:12:06 -0500

    Updated the userprofile - Sahithi Kasarapu

--- esparto/src/components/UserProfile/UserProfile.jsx
+++ esparto/src/components/UserProfile/UserProfile.jsx
@@ -1,11 +1,20 @@
 import {React, ReactDOM} from "react";
 
 var UserProfile = React.createClass({
-	//Sahithi
-    
-    
-    
-    
+	getDefaultProps: function() {
+		return({
+			person: {
+				name: 'Jack-Edward Oliver',
+				biography: '26 year old Designer / Developer living in Stockholm. Originally from Oxford, England. Love to make stuff.',
+			},
+			image: 'http://static1.squarespace.com/static/55acc005e4b098e615cd80e2/t/57b057398419c2c454f09924/1471025851733/',
+			quote: {
+				content: 'Beautiful things don\'t ask for attention',
+				source: 'The Secret Life of Walter Mitty'
+			}
+			
+		})
+	},
 	render: function() {
 		return(
 			<div className="App">
