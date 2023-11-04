9b91fc7bcd97ea04b901bda1ad22915e6a412192
AjayKumar1403 <ajaykumar.us21@gmail.com>
2023-11-03 20:44:08 -0500

    Made changes for the app.js for all the components to appear

--- esparto/src/App.js
+++ esparto/src/App.js
@@ -1,24 +1,35 @@
 import './App.css';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
-import Home from './components/Home/home';
 import { Route, Routes } from 'react-router-dom';
 import UserProfile from './components/UserProfile/UserProfile';
 import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
 import ParentContainer from './ParentContainer';
+import Paintings from './CollectionPages/Painting';
+import Drwaings from './CollectionPages/Drawing';
+import Sculptures from './CollectionPages/Sculpture';
+import Collage from './CollectionPages/Collage';
+import Photography from './CollectionPages/Photography';
+import ExcitingFeatures from './Whatsnew/ExcitingFeatures';
+import FeaturedArtists from './Whatsnew/FeaturedArtists';
+import NewArtworks from './Whatsnew/NewArtworks';
 function App() {
   return (
     <div className="App" style={{ background: '#4e54c8' }}>
       <Navbar />
-      {/* <Route exact path="/sidebar" Component={Sidebar} /> */}
-      {/* <Sidebar /> */}
       <Routes>
-        {/* <Route exact path="/" Component={Home} /> */}
         <Route exact path="/" element={<ParentContainer />} />
         <Route exact path="/login" Component={Login} />
         <Route exact path="/register" Component={Register} />
         <Route exact path="/userProfile" Component={UserProfile} />
+        <Route exact path="/paintings" Component={Paintings} />
+        <Route exact path="/photography" Component={Photography} />
+        <Route exact path="/collage" Component={Collage} />
+        <Route exact path="/sculpture" Component={Sculptures} />
+        <Route exact path="/drawings" Component={Drwaings} />
+        <Route exact path="/new-artworks" Component={NewArtworks} />
+        <Route exact path="/featured-artists" Component={FeaturedArtists} />
+        <Route exact path="/exciting-features" Component={ExcitingFeatures} />
       </Routes>
     </div>
   );
