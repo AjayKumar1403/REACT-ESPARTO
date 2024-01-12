082acefeaba4b82dd6dc7abf52509314d3a935e5
SahithiKasarapu <sahithikasarapu@gmail.com>
2024-01-12 10:59:10 -0600

    Added mongodb library to the API.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+
+
+
+
 app.use(express.json());
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB connection successfull"))
+    .catch((err) => console.log(err));
 
