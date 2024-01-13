2d99d856e54cdd9a30d304f8d1befdbf0a26d178
Sai Chaitanya Vishnu Kanth Inavolu <119522543+Sai-1305@users.noreply.github.com>
2024-01-12 19:16:38 -0600

    Sai Chaitanya Vishnu Kanth Inavolu: Defined Mongoose schema for 'Cart' in MongoDB

---
+++ esparto/src/Models/cart.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+  },
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
