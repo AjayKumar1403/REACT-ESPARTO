08d326f3471b45da62a5e635842bc721617d4242
ManojThella <manojthella98@gmail.com>
2024-01-22 08:25:07 -0600

    Added user registration endpoint

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,8 +4,16 @@ const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 //REGISTER
-//Manoj
-
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString(),
+    });
+});
  // Vishnu
 
 //LOGIN - Varsha
