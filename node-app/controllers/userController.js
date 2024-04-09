700081114200a966c002b8b8e8b2f7fa484de2f2
ManojThella <manojthella98@gmail.com>
2024-04-09 13:49:43 -0500

    updated

--- node-app/controllers/userController.js
+++ node-app/controllers/userController.js
@@ -1,24 +1,3 @@
-const mongoose = require('mongoose');
-var jwt = require('jsonwebtoken');
+//Ajay
 
-const Users = mongoose.model('Users', {
-    username: String,
-    mobile: String,
-    email: String,
-    password: String,
-    likedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Products' }]
-});
-
-module.exports.likeProducts = (req, res) => {
-    let productId = req.body.productId;
-    let userId = req.body.userId;
-
-    Users.updateOne({ _id: userId }, { $addToSet: { likedProducts: productId } })
-        .then(() => {
-            res.send({ message: 'liked success.' })
-        })
-        .catch(() => {
-            res.send({ message: 'server err' })
-        })
-
-}
+//Sahithi
