b4d2c704c9c5eaf9774b99d497f41bcf4e781998
AjayKumar1403 <ajaykumar.us21@gmail.com>
2024-04-12 00:24:37 -0500

    made changes to the userController file

--- node-app/controllers/userController.js
+++ node-app/controllers/userController.js
@@ -1,4 +1,111 @@
-//Ajay
+const mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+
+const Users = mongoose.model('Users', {
+    username: String,
+    mobile: String,
+    email: String,
+    password: String,
+    likedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Products' }],
+    image: {
+        type: String,
+        default: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80', // Default image URL
+    },
+
+    aboutMe: {
+        type: String,
+        default: 'No information available', // Default about me text
+    }
+});
+
+module.exports.likeProducts = (req, res) => {
+    let productId = req.body.productId;
+    let userId = req.body.userId;
+
+    Users.updateOne({ _id: userId }, { $addToSet: { likedProducts: productId } })
+        .then(() => {
+            res.send({ message: 'liked success.' })
+        })
+        .catch(() => {
+            res.send({ message: 'server err' })
+        })
+
+}
+
+module.exports.dislikeProducts = (req, res) => {
+    let productId = req.body.productId;
+    let userId = req.body.userId;
+
+    Users.updateOne({ _id: userId }, { $pull: { likedProducts: productId } })
+        .then(() => {
+            res.send({ message: 'Disliked success.' })
+        })
+        .catch(() => {
+            res.send({ message: 'server err' })
+        })
+
+}
+
+module.exports.signup = (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const mobile = req.body.mobile;
+    const user = new Users({ username: username, password: password, email, mobile });
+    user.save()
+        .then(() => {
+            res.send({ message: 'saved success.' })
+        })
+        .catch(() => {
+            res.send({ message: 'server err' })
+        })
+
+}
+
+module.exports.myProfileById = (req, res) => {
+    let uid = req.params.userId
+
+    Users.findOne({ _id: uid })
+        .then((result) => {
+            res.send({
+                message: 'success.', user: {
+                    email: result.email,
+                    mobile: result.mobile,
+                    username: result.username,
+                    aboutMe: result.aboutMe,
+                    password: result.password,
+                    image : result.image,
+                }
+            })
+        })
+        .catch(() => {
+            res.send({ message: 'server err' })
+        })
+
+    return;
+
+}
+
+module.exports.getUserById = (req, res) => {
+    const _userId = req.params.uId;
+    Users.findOne({ _id: _userId })
+        .then((result) => {
+            res.send({
+                message: 'success.', user: {
+                    email: result.email,
+                    mobile: result.mobile,
+                    username: result.username,
+                    aboutMe: result.aboutMe,
+                    password: result.password,
+                    image : result.image,
+
+                }
+            })
+        })
+        .catch(() => {
+            res.send({ message: 'server err' })
+        })
+}
 
 module.exports.login = (req, res) => {
     const username = req.body.username;
